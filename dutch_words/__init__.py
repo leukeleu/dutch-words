af43b611597b4490cfe0997c38421f0257dd9435
Ramon de Jezus <rdejezus@leukeleu.nl>
2019-12-18 14:47:12 +0100

    Don't use file as variable

--- dutch_words/__init__.py
+++ dutch_words/__init__.py
@@ -7,5 +7,5 @@ import os
 def get_ranked():
     path = os.path.dirname(os.path.abspath(__file__))
 
-    with open(os.path.join(path, 'dutch10000-utf8.txt')) as file:
-        return file.read().splitlines()
+    with open(os.path.join(path, 'dutch10000-utf8.txt')) as word_list:
+        return word_list.read().splitlines()
